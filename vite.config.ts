d83ded58bc80158c8da483ae1c1dfb96eb680385
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-08-30 09:05:54 -0400

    fixed gh-pages router error handling

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/urban-espionage/'
+    base: process.env.NODE_ENV === 'production' ? '/urban-espionage/' : '/'
 })
